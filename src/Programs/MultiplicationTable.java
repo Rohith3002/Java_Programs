631eedde9ee9501ea494db526689688c23b4764a
Rohith3002 <rohith.3002@gmail.com>
2023-12-27 20:08:05 +0530

    Palindrome

---
+++ src/Programs/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package Programs;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(a+ " x " +i+" = "+a*i);
+        }
+    }
+}
--- src/Programs/Palindrome.java
+++ src/Programs/Palindrome.java
@@ -1,4 +1,30 @@
 package Programs;
 
+import java.util.Scanner;
+
 public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), rev = 0, remainder;
+
+        // store the number to originalNum
+        int originalNum = a;
+        // get the reverse of originalNum
+        // store it in variable
+        while (a != 0) {
+            remainder = a % 10;
+            rev = rev * 10 + remainder;
+            a /= 10;
+        }
+
+        // check if reversedNum and originalNum are equal
+        if (originalNum == rev) {
+            System.out.println(originalNum + " is Palindrome.");
+        } else {
+            System.out.println(originalNum + " is not Palindrome.");
+
+        }
+
+    }
 }
